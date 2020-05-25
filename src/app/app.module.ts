3a2fdea6e3f4be0cac575c36d90b2edccc48aa66
Srikanth Vinnamala <srikanthvinnamala@gmail.com>
2020-05-25 21:16:15 +0530

    first submit by Srikanth Vinnamala

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
 import { PizzaDashboardModule } from './pizza-dashboard/pizza-dashboard.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { PizzaDashboardModule } from './pizza-dashboard/pizza-dashboard.module';
     CoreModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-
+    HttpClientModule,
     PizzaDashboardModule,
     MatSidenavModule,
   ],
--- src/app/pizza-logistics/pizza-logistics.module.ts
+++ src/app/pizza-logistics/pizza-logistics.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -8,10 +8,13 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { PizzaLogisticsRoutingModule } from './pizza-logistics-routing.module';
 import { PizzaLogisticsComponent } from './pizza-logistics/pizza-logistics.component';
+import { MatGridListModule, MatSnackBarModule } from '@angular/material';
+import { filterPipe } from '../shared/pipe/filter.pipe';
 
 @NgModule({
   declarations: [
     PizzaLogisticsComponent,
+    filterPipe
   ],
   imports: [
     CommonModule,
@@ -22,7 +25,9 @@ import { PizzaLogisticsComponent } from './pizza-logistics/pizza-logistics.compo
     MatSelectModule,
     MatButtonModule,
     MatInputModule,
-    MatButtonModule,
+    MatGridListModule,
+    FormsModule,
+    MatSnackBarModule
   ],
 })
 export class PizzaLogisticsModule { }
